f21876652bb920b4bb34a201f479d446a40ff6c6
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-17 17:22:15 +0200

    update page.tsx

--- components/src/app/page.tsx
+++ components/src/app/page.tsx
@@ -1,18 +1,9 @@
 import React from "react";
 import {MainLandingPage}  from '../components/'
-
-
-
-
-
 export default function Home() {
   return (
     <div className="">
     <MainLandingPage session={undefined} />
-    
-     
-
-
     </div>
 
     
