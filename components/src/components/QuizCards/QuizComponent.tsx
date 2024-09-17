ba664babfe72fe88ce5dfb6ca90b162b4160ada2
Dzunisan <mongwed@gmail.com>
2024-09-17 19:02:12 +0200

    update QuizComponent.tsx

--- components/src/components/QuizCards/QuizComponent.tsx
+++ components/src/components/QuizCards/QuizComponent.tsx
@@ -3,7 +3,8 @@
 import { useParams, useRouter } from "next/navigation";
 import React, { useState, useRef, useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
-import ScoreModal from './ScoreModal';
+import ScoreModal from "./ScoreModal";
+import { PiHourglassFill } from "react-icons/pi";
 
 type Question = {
   question: string;
@@ -33,7 +34,7 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
   const [showAnswer, setShowAnswer] = useState(false);
   const [score, setScore] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [timeLeft, setTimeLeft] = useState(30); 
+  const [timeLeft, setTimeLeft] = useState(30);
 
   const timerRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -42,7 +43,7 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
       moveToNextQuestion();
       return;
     }
-    
+
     timerRef.current = setInterval(() => {
       setTimeLeft((prevTime) => prevTime - 1);
     }, 1000);
@@ -87,7 +88,7 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
-    router.push('/cards');
+    router.push("/cards");
   };
 
   return (
@@ -97,7 +98,7 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
         style={{ backgroundImage: `url(${file.source})` }}
       >
         <button
-          className="absolute top-4 right-4 bg-red-500 p-2 rounded-full text-white text-3xl"
+          className="absolute top-4 right-4 bg-red-600 p-2 rounded-full text-white text-3xl"
           onClick={() => router.push("/cards")}
         >
           <FaTimes />
@@ -105,12 +106,15 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
 
         <div className="flex items-center justify-center h-full w-full">
           <div className="relative rounded-lg shadow-lg max-w-md w-full">
-            <div className="bg-cover bg-center rounded-t-lg p-6" style={{ backgroundColor: file.bgColor }}>
+            <div
+              className="bg-cover bg-center rounded-t-lg p-6"
+              style={{ backgroundColor: file.bgColor }}
+            >
               <h2 className="text-2xl font-bold text-center text-white mb-2">
                 {file.title} Questions
               </h2>
               <p className="text-center text-white">
-                {currentQuestion?.question ?? 'Loading question...'}
+                {currentQuestion?.question ?? "Loading question..."}
               </p>
             </div>
             <div className="bg-white p-6 rounded-b-lg space-y-4">
@@ -119,9 +123,11 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
                   key={index}
                   className="w-full py-2 px-4 border-4 rounded-2xl hover:bg-green-300"
                   style={{
-                    borderColor: selectedOption === option ? 'green' : file.bgColor,
-                    backgroundColor: selectedOption === option ? 'green' : 'transparent',
-                    color: selectedOption === option ? 'white' : 'black'
+                    borderColor:
+                      selectedOption === option ? "green" : file.bgColor,
+                    backgroundColor:
+                      selectedOption === option ? "green" : "transparent",
+                    color: selectedOption === option ? "white" : "black",
                   }}
                   onClick={() => handleAnswerCheck(option)}
                 >
@@ -138,9 +144,21 @@ export const QuizComponent: React.FC<FileDetailProps> = ({ files }) => {
                 )}
               </div>
             </div>
-            <p className={`text-center text-lg mt-6`} style={{ backgroundColor: file.bgColor }}>
-              <span className="font-bold">{timeLeft}s</span>
-            </p>
+            <div
+              className="flex items-center justify-center text-lg mt-6 p-6 rounded-lg space-x-4"
+              style={{ backgroundColor: file.bgColor }}
+            >
+              <PiHourglassFill
+                className="h-10 w-10 rounded-full text-white border-4 border-white p-2"
+                style={{ backgroundColor: file.bgColor }}
+              />
+              <span
+                className="font-semibold text-base p-2 h-10 w-10 flex items-center justify-center rounded-full text-white border-4 border-white"
+                style={{ backgroundColor: file.bgColor }}
+              >
+                {timeLeft}s
+              </span>
+            </div>
           </div>
         </div>
       </div>
