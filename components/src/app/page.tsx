280f23e01090808dcd5269b25b4d4246cde10d7d
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-17 16:37:05 +0200

    update page.tsx

--- components/src/app/page.tsx
+++ components/src/app/page.tsx
@@ -1,24 +1,10 @@
-import { ExampleComponent, QuizComponent } from "@/components";
+import { ExampleComponent, files, QuizComponent } from "@/components";
 
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     {
-      [{bg: '#243975', title: 'DIVERSITY QUESTIONS'}, {
-        bg: '#9c27b0',
-        title: 'STORY TELLING'
-      },
-      {
-        bg: '#1de9b6',
-        title: 'UNITY CARDS'
-      },
-      {bg: '#29b6f6', title: 'CHALENGE CARDS'}
-    ].map(({bg, title}, i) => (
-        <ExampleComponent key={i} bg={bg} title={title} />
-      ))
-     }
-     <QuizComponent files={[]}/>
+     <QuizComponent files={files}/>
     </div>
   );
 }
