90a590e7ff62255581c399caf2ebb0a1b2a770c8
Dzunisan <mongwed@gmail.com>
2024-09-17 13:18:51 +0200

    feat: create StaticData.ts

---
+++ components/src/components/QuizCards/StaticData.ts
@@ -0,0 +1,34 @@
+export const files = [
+    {
+      id:"1",
+      title: "DIVERSITY QUESTIONS",
+      source:
+        "/cards/blue.jpeg",
+      bgColor: "bg-[#1A3E75]",
+    },
+    {
+      title: "STORYTELLING PROMPTS",
+      id:"2",
+  
+      source:
+      "/cards/pink.jpeg",
+      bgColor: "bg-[#B32891]",
+    
+  
+    },
+    {
+      title: "CHALLENGING CARDS",
+      source:
+      "/cards/light.webp",
+      bgColor: "bg-[#00C8DA]",
+      id:"3",
+  
+    },
+    {
+      title: "UNITY CARDS",
+      source:
+      "/cards/green.jpeg",
+      bgColor: "bg-[#00BEA1]",
+      id:"3",
+    },
+  ];
