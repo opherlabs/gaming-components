e915b9ab922f26ee7e4ccf860ae55af9dc3dc518
Dzunisan <mongwed@gmail.com>
2024-09-17 16:11:10 +0200

    update page.tsx

--- components/src/app/cards/page.tsx
+++ components/src/app/cards/page.tsx
@@ -1,12 +1,25 @@
 import { files, QuizCards } from "@/components";
-import React from "react";
 
 function CardsComponent() {
-  return (
-    <div>
-        <QuizCards files={files}/>
-    </div>
-  );
-}
-
-export default CardsComponent;
+    return (
+      <div
+        style={{
+          backgroundImage: 'url("/cards/flag.svg")',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100vh', 
+          width: '100vw',  
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <div className="p-4 m-6 rounded-lg shadow-md w-full bg-white">
+          <QuizCards files={files} />
+        </div>
+      </div>
+    );
+  }
+  
+  export default CardsComponent;
+  
