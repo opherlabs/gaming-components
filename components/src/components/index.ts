315a97d54b5e9b07bd81956bda23249adb276f6b
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-17 17:58:11 +0200

    update components/src/components/index.ts

--- components/src/components/index.ts
+++ components/src/components/index.ts
@@ -2,3 +2,4 @@
 export * from './QuizCards';
 export {ExampleComponent} from './ExampleComponent'
 export {MainLandingPage } from './LandingPage'
+export {CompanyValuesComponent} from './CompanyValuesComponent'
