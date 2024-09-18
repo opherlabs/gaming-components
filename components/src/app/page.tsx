662e07b3988ccf42107d9120bdc522dca493b73d
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-18 18:17:36 +0200

    update page.tsx

--- components/src/app/page.tsx
+++ components/src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AnimatedCard, MainLandingPage, QuizCards } from '../components/'
+import { MainLandingPage, QuizCards } from '../components/'
 import { files } from "@/components/QuizCards";
 
 export default function Home() {
