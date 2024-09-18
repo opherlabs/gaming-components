d35127860b191019dd7c0f7c3732d68c8586ddeb
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-18 08:42:45 +0200

    fix updates

--- components/src/app/page.tsx
+++ components/src/app/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { MainLandingPage,files, CompanyValuesComponent,QuizComponent, QuizCards } from '../components/'
+import { MainLandingPage, QuizCards } from '../components/'
+import { files } from "@/components/QuizCards";
 
 export default function Home() {
   return (
     <div className="">
-      {/* <MainLandingPage session={null} /> */}
-      {/* <CompanyValuesComponent /> */}
+      <MainLandingPage session={null} />
       <QuizCards files={files}       />
     </div>
 
