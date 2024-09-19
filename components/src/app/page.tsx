66fd6a22b8cdbb420ba85b898c1705f0ec3266da
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 07:43:14 +0200

    update page.tsx

--- components/src/app/page.tsx
+++ components/src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { CountdownComponent, MainLandingPage } from '../components/'
+import { CountdownComponent } from '../components/'
 
 export default function Home() {
   return (
     <div className="">
-      {/* <MainLandingPage session={null} /> */}
       <CountdownComponent url='/api/auth/signin'  targetDate={'2024-09-26T12:00:00'} />
     </div>
     
