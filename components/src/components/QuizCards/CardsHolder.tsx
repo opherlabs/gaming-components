87e9d5c3b12e6dd5379633ff2ec274f4d089378a
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 02:51:41 +0200

    add components/src/components/QuizCards/CardsHolder.tsx for rendering quiz card layout with a background image and children elements

---
+++ components/src/components/QuizCards/CardsHolder.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const CardsHolder = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div>
+            <div
+                style={{
+                    backgroundImage: 'url("/cards/flag.svg")',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '100vh',
+                    width: '100vw',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <div className="p-4 m-6 rounded-lg shadow-md w-full bg-white">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
