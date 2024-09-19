d5b1a349d23c0f2bfa72fbbb330404cff37693c5
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 16:28:45 +0200

    update RegisterAction.tsx

--- components/src/components/CountDown/RegisterAction.tsx
+++ components/src/components/CountDown/RegisterAction.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 
 export const RegisterAction: React.FC<{url:string, mode?: 'session'|'register', session?: IUserSession}> = ({url, mode="register", session}) => {
+  console.log(session)
   return (
     <div>
         <Link href={mode === 'register' ? url : 'public' + '?mode=test'} className="mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-3 px-4 rounded">
