a95b023dbf2bb10ae596ac9bad0e066c2e089034
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 02:51:29 +0200

    update page.tsx

--- components/src/app/cards/page.tsx
+++ components/src/app/cards/page.tsx
@@ -1,27 +1,15 @@
 import React from "react";
-import {  QuizCards } from "../../components";
-import { files } from "@/components/QuizCards";
+import { QuizCards } from "../../components";
+import { CardsHolder, files } from "@/components/QuizCards";
 
 function CardsComponent() {
-    return (
-      <div
-        style={{
-          backgroundImage: 'url("/cards/flag.svg")',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          height: '100vh', 
-          width: '100vw',  
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <div className="p-4 m-6 rounded-lg shadow-md w-full bg-white">
-          <QuizCards url="cards" files={files} />
-        </div>
-      </div>
-    );
-  }
-  
-  export default CardsComponent;
-  
+  return (
+    <CardsHolder>
+      <QuizCards url="cards" files={files} />
+    </CardsHolder>
+
+
+  );
+}
+
+export default CardsComponent;
